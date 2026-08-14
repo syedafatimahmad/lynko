7f1ed67fdeff3ea884368841368ed1c63d256457
Your Name <you@example.com>
2026-08-14 23:45:48 +0500

    Update start.bat to auto-install dependencies

--- start.bat
+++ start.bat
@@ -1,5 +1,16 @@
 @echo off
-echo Starting Lynko Chain of Custody App...
+echo ========================================
+echo Starting Lynko App...
+echo ========================================
+
+IF NOT EXIST "node_modules\" (
+    echo First time running on this computer!
+    echo Installing required dependencies... this may take a minute.
+    call npm install
+)
+
+echo.
+echo Starting the development server...
 echo ----------------------------------------
-call npx expo start --host lan -c
+call npx expo start -c
 pause
